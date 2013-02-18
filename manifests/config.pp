0840d4776140f37c6ed60d4c374b8d217431e565
Justin Lambert <jlambert@eml.cc>
2013-02-18 12:20:17 -0700

    align defaults for easier readibility

--- manifests/config.pp
+++ manifests/config.pp
@@ -18,26 +18,27 @@
 #
 # * Update documentation
 #
-class logstash::config( $logstash_home = '/usr/local/logstash',
-  $logstash_etc = '/etc/logstash',
-  $logstash_log = '/var/log/logstash',
-  $logstash_transport = 'amqp',
-  $logstash_jar_provider = 'package',
-  $logstash_version = '1.1.5',
-  $logstash_verbose = 'no',
-  $logstash_user  = 'logstash',
-  $logstash_group = 'logstash',
+class logstash::config( 
+  $logstash_home          = '/usr/local/logstash',
+  $logstash_etc           = '/etc/logstash',
+  $logstash_log           = '/var/log/logstash',
+  $logstash_transport     = 'amqp',
+  $logstash_jar_provider  = 'package',
+  $logstash_version       = '1.1.5',
+  $logstash_verbose       = 'no',
+  $logstash_user          = 'logstash',
+  $logstash_group         = 'logstash',
   $elasticsearch_provider = 'external',
-  $elasticsearch_host = '127.0.0.1',
-  $redis_provider = 'external',
-  $redis_package = 'redis',
-  $redis_version = '2.4.15',
-  $redis_host = '127.0.0.1',
-  $redis_port = '6379',
-  $redis_key = 'logstash',
-  $java_provider = 'package',
-  $java_package = 'java-1.6.0-openjdk',
-  $java_home = '/usr/lib/jvm/jre-1.6.0-openjdk.x86_64'
+  $elasticsearch_host     = '127.0.0.1',
+  $redis_provider         = 'external',
+  $redis_package          = 'redis',
+  $redis_version          = '2.4.15',
+  $redis_host             = '127.0.0.1',
+  $redis_port             = '6379',
+  $redis_key              = 'logstash',
+  $java_provider          = 'package',
+  $java_package           = 'java-1.6.0-openjdk',
+  $java_home              = '/usr/lib/jvm/jre-1.6.0-openjdk.x86_64'
 ) {
 
   # just trying to make the fq variable a little less rediculous
--- manifests/javainitscript.pp
+++ manifests/javainitscript.pp
@@ -1,14 +1,14 @@
 # templated java daemon script
 define logstash::javainitscript (
-  $servicename = $title,
+  $servicename      = $title,
   $serviceuser,
-  $servicegroup = $serviceuser,
+  $servicegroup     = $serviceuser,
   $servicehome,
-  $serviceuserhome = $servicehome,
+  $serviceuserhome  = $servicehome,
   $servicelogfile,
   $servicejar,
   $serviceargs,
-  $java_home = '/usr/lib/jvm/jre-1.6.0-openjdk.x86_64'
+  $java_home        = '/usr/lib/jvm/jre-1.6.0-openjdk.x86_64'
 ) {
 
   file { "/etc/init.d/${servicename}":
