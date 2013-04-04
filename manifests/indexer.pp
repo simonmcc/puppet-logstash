9a9e68260486b81f81e879aa89d55ea67b4bfab2
Malte Janduda <malte.janduda@hybris.com>
2013-04-04 02:43:37 +0200

    code orientation fixed

--- manifests/indexer.pp
+++ manifests/indexer.pp
@@ -40,7 +40,7 @@ class logstash::indexer (
     /^amqp$/:  { $indexer_conf_content = template('logstash/indexer-input-amqp.conf.erb',
                                                   'logstash/indexer-filter.conf.erb',
                                                   'logstash/indexer-output.conf.erb') }
-    /^log4j$/:  { $indexer_conf_content = template('logstash/indexer-input-log4j.conf.erb',
+    /^log4j$/: { $indexer_conf_content = template('logstash/indexer-input-log4j.conf.erb',
                                                   'logstash/indexer-filter.conf.erb',
                                                   'logstash/indexer-output.conf.erb') }
     default:   { $indexer_conf_content = template('logstash/indexer-input-amqp.conf.erb',
